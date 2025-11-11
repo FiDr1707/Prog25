dda6ec2db4dae0b02b3af20a900e03c3fac829ac
rost <dronovfiloposhka@gmail.com>
2025-11-11 12:56:24 +0700

    Добавили два новых конструктора в класс Queue и продемонстрировали их работу. Программа не работает, 17 ошибок

--- QDemo.java
+++ QDemo.java
@@ -1,11 +1,32 @@
 class Queue {
-  char q[]; //ссылка на массив, хранящий элементы очереди
-  int putloc, getloc; //укахатели на начало и конец очереди
+  private char q[]; //ссылка на массив, хранящий элементы очереди
+  private int putloc, getloc; //укахатели на начало и конец очереди
 
+  //Конструктор для создания пустой очереди
   Queue (int size){
     q = new char [size + 1]; //выделение памяти для массива
     putloc = getloc = 0;
   }
+
+  //Конструктор для копирования существующей очереди в новую очередь
+  Queue(Queue ob) {
+	  putloc = ob.putloc;
+	  getloc = ob.getloc;
+	  q = new char [ob.q.length];
+
+	  for (int i = getloc+1; i <= putloc; i++)
+		  q[i] = ob.q[i];
+  }
+  //Конструктор для помещения значений из массива в новую очередь
+  Queue(char a[]) {
+	putloc = 0;
+	getloc = 0;
+	q = new char[a.length+1];
+
+	for(int i = 0; i < a.length; i++)
+		put(a[i]);
+  }
+
     //Метод для помещения символа в очередь
 
     void put(char ch) {
@@ -39,11 +60,14 @@ class Queue {
         bigQ.put((char) ('А' + i));
 
       System.out.print("Содержимое очереди bigQ: ");
+
       for (i = 0; i < 33; i++)
         ch = bigQ.get();
+
       if (ch != (char) 0)
         System.out.print(ch);
     }
+
     System.out.println("\n");
 
     System.out.println("Использование очереди smallQ для демонстрации переполнения ");
@@ -61,6 +85,59 @@ class Queue {
       ch = smallQ.get();
       if (ch != (char) 0)
         System.out.print(ch);
-    }
+    System.out.println(" Попытка извлечения символа из очереди smallQ: ");
+  }
+
+  for (i =0; i < 5; i++) {
+	System.out.println("Попытка сохранения " + (char) ('Я' - i));
+
+	smallQ.put((char) ('Я' - i));
+	System.out.println();
+
+	//Попытка доступа к закрытым элементам очереди через индекс
+	Queue test = new Queue(10);
+	test.put('A');
+	System.out.println("Извлечение символа из очереди test " + test.get());
+  }
+
+  //Создание пустой очереди
+  Queue q1 = new Queue(10);
+
+  char arr1[] = {'Q', 'W', 'E', 'R', 'T', 'Y'};
+  //Создание очереди на основе массива arr1[]
+  Queue = q2 = new Queue(arr1);
+
+  //Помещение символов в очередь q1
+  for(i = 0; i < 10; i++)
+	  q1.put((char) ('Y' + i));
+
+  //Создание очереди q3 на основе очереди q1
+  Queue q3 = new Queue(q1);
+
+  //Отображаем содержимое очередей q1, q2, q3
+  //
+  System.out.println();
+
+  System.out.println("Очередь q1: ");
+  for (i = 0; i<10; i++{
+	ch = q1.get();
+	System.out.println(ch);
+  }
+  System.out.println();
+
+  System.out.println("Очередь q2: ");
+  for (i = 0; i<10; i++{
+	ch = q2.get();
+	System.out.println(ch);
+  }
+  System.out.println();
+
+  System.out.println("Очередь q3: ");
+  for (i = 0; i<10; i++{
+	ch = q3.get();
+	System.out.println(ch);
+  }
+  System.out.println();
+  }
   }
 }
